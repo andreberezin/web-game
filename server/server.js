58545824e02d0fa5443e1512786dcab17fec38d5
Andre Berezin <andreberezin@outlook.com>
2025-05-31 11:19:31 +0300

    feat: added homepage for backend to get rid of error

--- server/server.js
+++ server/server.js
@@ -8,18 +8,26 @@ const server = http.createServer(app);
 
 const port = process.env.BACKEND_PORT || 3000;
 
+// Socket.io
+const io = new Server(server, {
+    cors: {
+        origin: ['http://localhost:5173', 'https://just-panda-musical.ngrok-free.app', 'https://intimate-upright-sunfish.ngrok-free.app'],
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+        credentials: true
+    },
+});
+
 // CORS
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://just-panda-musical.ngrok-free.app'],
+    origin: ['http://localhost:5173', 'https://just-panda-musical.ngrok-free.app', 'https://intimate-upright-sunfish.ngrok-free.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
 }));
 
+app.use(express.static('public'));
+
 app.use(express.json());
 
-// Socket.io
-const io = new Server(server, {
-    cors: { origin: ['http://localhost:5173', 'https://just-panda-musical.ngrok-free.app'] }
-});
 
 io.on('connection', (socket) => {
     console.log('🔌 User connected');
