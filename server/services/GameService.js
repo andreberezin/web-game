4bec217764ca005fab77450cfd56ae3046431354
Marius Soon <mariussoon2@gmail.com>
2025-07-13 17:55:54 +0300

    Refactored updateBullets method in GameService and created helper methods

--- server/services/GameService.js
+++ server/services/GameService.js
@@ -4,80 +4,102 @@ import {clamp} from "../utils/clamp.js";
 
 export class GameService {
 
-	#gamesManager;
+    #gamesManager;
 
-	constructor(playerInputService) {
-		this.playerInputService = playerInputService;
+    static GAME_BOUNDS = {
+        MIN_X: 0,
+        MAX_X: 1000,
+        MIN_Y: 0,
+        MAX_Y: 1000
+    }
+
+    constructor(playerInputService) {
+        this.playerInputService = playerInputService;
+    }
+
+    setGamesManager(gamesManager) {
+        this.#gamesManager = gamesManager;
+    }
+
+    createGame(hostId, settings) {
+        return new Game(hostId, settings);
+    }
+
+    updateGameState(game) {
+
+        this.updateBullets(game);
+
+        // handle player input
+        this.playerInputService.handlePlayerMovement(game);
+        this.playerInputService.handlePlayerShooting(game);
+
+    }
+
+    addPlayerToGame(gameId, playerId, player) {
+        const game = this.#gamesManager.games.get(gameId);
+        if (!game) {
+            return {success: false, reason: 'Game not found'};
+        }
+
+        let result;
+
+        if (Object.keys(game.getState.players).length < game.getSettings.maxPlayers) {
+            game.getState.players[playerId] = player;
+            result = true;
+        } else {
+            result = false;
+        }
+
+        // if (result) {
+        // 	this.broadcastPlayerJoined(gameId, playerId, result.player);
+        // }
+
+        return result;
+    }
+
+    createBulletAt(x, y, direction, game) {
+        const id = Math.floor(Math.random() * 10000);
+        const bullet = new Bullet(id, x, y, direction);
+        game.getState.bullets[id] = bullet;
+    }
+
+    updateBullets(game) {
+        const bullets = game.getState.bullets;
+        const directionMap = {
+            "up": {coord: "y", multiplier: -1},
+            "down": {coord: "y", multiplier: 1},
+            "left": {coord: "x", multiplier: -1},
+            "right": {coord: "x", multiplier: 1}
+        }
+        const bulletsToDelete = [];
+
+        Object.entries(bullets).forEach(([bulletId, bullet]) => {
+            const directionValues = directionMap[bullet.direction];
+
+            this.moveBulletByVelocity(bullet, directionValues);
+
+            if (this.isOutOfBounds(bullet.pos)) {
+                bulletsToDelete.push(bulletId);
+            }
+        });
+
+        this.deleteBulletsIfOutOfBounds(bulletsToDelete, bullets);
+    }
+
+	moveBulletByVelocity(bullet, directionValues) {
+            if (directionValues) {
+                bullet.pos[directionValues.coord] += bullet.velocity * directionValues.multiplier;
+            }
 	}
 
-	setGamesManager(gamesManager) {
-		this.#gamesManager = gamesManager;
+	deleteBulletsIfOutOfBounds(bulletsToDelete, bullets) {
+        bulletsToDelete.forEach(bulletId => {
+            delete bullets[bulletId];
+        });
 	}
 
-	createGame(hostId, settings) {
-		return new Game(hostId, settings);
-	}
-
-	updateGameState(game) {
-
-		this.updateBullets(game);
-
-		// handle player input
-		this.playerInputService.handlePlayerMovement(game);
-		this.playerInputService.handlePlayerShooting(game);
-
-	}
-
-	addPlayerToGame(gameId, playerId, player) {
-		const game = this.#gamesManager.games.get(gameId);
-		if (!game) {
-			return { success: false, reason: 'Game not found' };
-		}
-
-		let result;
-
-		if (Object.keys(game.getState.players).length < game.getSettings.maxPlayers) {
-			game.getState.players[playerId] = player;
-			result = true;
-		}
-		result = false;
-
-		// if (result) {
-		// 	this.broadcastPlayerJoined(gameId, playerId, result.player);
-		// }
-
-		return result;
-	}
-
-	createBulletAt(x, y, direction, game) {
-		const id = Math.floor(Math.random() * 10000);
-		const bullet = new Bullet(id, x, y, direction);
-		game.getState.bullets[id] = bullet;
-	}
-
-	updateBullets(game) {
-		const bullets = game.getState.bullets;
-
-		for(let bulletId in bullets) {
-			const bullet = bullets[bulletId];
-
-				if (bullet.direction === "up") {
-					bullet.pos.y = bullet.pos.y - bullet.velocity;
-				}
-				if (bullet.direction === "down") {
-					bullet.pos.y = bullet.pos.y + bullet.velocity;
-				}
-				if (bullet.direction === "left") {
-					bullet.pos.x = bullet.pos.x - bullet.velocity;
-				}
-				if (bullet.direction === "right") {
-					bullet.pos.x = bullet.pos.x + bullet.velocity;
-				}
-
-				if (bullet.pos.y < 0 || bullet.pos.y > 1000 || bullet.pos.x < 0 || bullet.pos.x > 1000) {
-					delete bullets[bulletId];
-				}
-		}
-	}
-
+    isOutOfBounds(pos) {
+        const { MIN_X, MAX_X, MIN_Y, MAX_Y } = GameService.GAME_BOUNDS;
+        return pos.y < MIN_Y || pos.y > MAX_Y || pos.x < MIN_X || pos.x > MAX_X;
+    }
 }
