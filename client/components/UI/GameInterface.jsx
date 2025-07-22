3b4245b70a7426a4297c779246b9267b770fe4dc
Andre Berezin <andreberezin@outlook.com>
2025-07-22 14:50:00 +0300

    moved eslint config to root so /server and /client are both linted. Fixed eslint error related to new flat config style

--- client/components/UI/GameInterface.jsx
+++ client/components/UI/GameInterface.jsx
@@ -1,4 +1,4 @@
 
-export function GameInterface({gameInterface}) {
+export function GameInterface() {
 
 }
---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+// export default defineConfig([
+//   { ignores: ['dist'] },
+//   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+//   { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+//   pluginReact.configs.flat.recommended,
+// ]);
+
+export default defineConfig([
+  { ignores: ['dist'] },
+
+  pluginReact.configs.flat.recommended,
+
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+])
