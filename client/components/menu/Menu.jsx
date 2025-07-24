315f69bcee217789c516e24a2b0152c269318acb
Andre Berezin <andreberezin@outlook.com>
2025-07-24 17:04:42 +0300

    added live public games list to menu and differentiated public and private games

--- client/components/menu/Menu.jsx
+++ client/components/menu/Menu.jsx
@@ -4,7 +4,7 @@ import {useState} from 'react';
 import {CreateGame} from './CreateGame.jsx';
 
 export function Menu({clientManager, isGameStarted, setIsGameStarted}) {
-	const [isJoinGame, setIsJoinGame] = useState(false);const [isCreateGame, setIsCreateGame] = useState(false);
+	const [isCreateGame, setIsCreateGame] = useState(false);
 
 	// todo add logic to save game id to browser storage to automatically join correct game when refreshing
 
@@ -12,43 +12,26 @@ export function Menu({clientManager, isGameStarted, setIsGameStarted}) {
 	return(
 		<div id={"menu"}>
 
-			{!isJoinGame && !isCreateGame && !isGameStarted &&
-				<div className={"menu-items"}>
+			{!isCreateGame && !isGameStarted &&
+				<>
+
+					<JoinGame clientManager={clientManager} setIsGameStarted={setIsGameStarted}/>
+
 					<button id={"create-game"} onClick={() => {
-						setIsJoinGame(false);
 						setIsCreateGame(true);
 					}}
 					>
 						Create game
 					</button>
-					<button id={"join-game"} onClick={() => {
-						setIsCreateGame(false);
-						setIsJoinGame(true);
-					}
-					}
-					>
-						Join game
-					</button>
-				</div>
-			}
-
-			{isJoinGame && !isGameStarted &&
-				<JoinGame
-					clientManager={clientManager}
-					setIsJoinGame={setIsJoinGame}
-					isGameStarted={isGameStarted}
-					setIsGameStarted={setIsGameStarted}
-				/>
+				</>
 			}
 
 			{isCreateGame && !isGameStarted &&
 				<CreateGame
 					clientManager={clientManager}
 					setIsCreateGame={setIsCreateGame}
-					isGameStarted={isGameStarted}
 					setIsGameStarted={setIsGameStarted}
 				/>
-				// CreateGame(setIsCreateGame, isGameStarted, setIsGameStarted)
 			}
 
 		</div>
