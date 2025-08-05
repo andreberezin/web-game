90c9ab97b494e7203b4702f3316ba06f635ef221
Andre Berezin <andreberezin@outlook.com>
2025-08-05 09:52:35 +0300

    WG-57 | moved react games state up to maintain available games data when rendering subcomponent

--- client/components/menu/Menu.jsx
+++ client/components/menu/Menu.jsx
@@ -1,6 +1,6 @@
 import '../../styles/menu.scss'
 import {JoinGame} from './JoinGame.jsx';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {CreateGame} from './CreateGame.jsx';
 import {CreatePlayer} from './CreatePlayer.jsx';
 
@@ -10,6 +10,23 @@ export function Menu({clientManager, isGameStarted, setIsGameStarted}) {
 	const [gameId, setGameId] = useState(null);
 
 	// todo add logic to save game id to browser storage to automatically join correct game when refreshing
+
+	const [games, setGames] = useState({});
+
+	useEffect(() => {
+		//const socket = clientManager.socketHandler.socket;
+
+		clientManager.socketHandler.on("updateAvailableGames", (gamesList) => {
+			const gamesObj = Object.fromEntries(gamesList.map((game) => [game.id, game]));
+
+			setGames(gamesObj);
+		});
+
+		return () => {
+			clientManager.socketHandler.on("updateAvailableGames", null);
+		}
+
+	}, [clientManager.socketHandler]); // empty deps now fine because no external dependencies used in handlers
 
 
 	return(
@@ -22,6 +39,7 @@ export function Menu({clientManager, isGameStarted, setIsGameStarted}) {
 						setIsCreatePlayer={setIsCreatePlayer}
 						setGameId={setGameId}
 						gameId={gameId}
+						games={games}
 					/>
 
 					<div className={"menu-item"} id={"create-game-button-container"}>
