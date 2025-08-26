3527cffca0add9540f4f761fcd5e85a5b0caf66d
Andre Berezin <andreberezin@outlook.com>
2025-08-26 15:48:08 +0300

    WG-60 | moved code, added functions to handle game state changes

--- client/services/GameService.js
+++ client/services/GameService.js
@@ -1,14 +1,96 @@
-import {existingUI} from '../utils/existingUI.js';
+import Player from '../models/Player.js';
 
 export default class GameService {
 	#clientStore;
 	#socketHandler;
 	#gameFieldService;
+	#clientManager;
+	#playerService;
+	#playerInterfaceService;
+	#gameInterfaceService;
 
-	constructor({clientStore, socketHandler, gameFieldService}) {
+	constructor({clientStore, socketHandler, gameFieldService, playerService, playerInterfaceService, gameInterfaceService}) {
 		this.#clientStore = clientStore;
 		this.#socketHandler = socketHandler;
 		this.#gameFieldService = gameFieldService;
+		this.#playerService = playerService;
+		this.#playerInterfaceService = playerInterfaceService;
+		this.#gameInterfaceService = gameInterfaceService;
+	}
+
+	setClientManager(clientManager) {
+		this.#clientManager = clientManager;
+	}
+
+	initializePlayers(gameId, players, myPlayer) {
+		if (!this.#clientStore.myId) {
+			this.#clientStore.myId = myPlayer.id;
+		}
+
+		const gameState = this.#clientStore.games.get(gameId).state;
+		let currentIndex = Object.keys(gameState.players).length
+
+		for (const playerID in players) {
+			// if (!gameState.players[playerID]) {
+			currentIndex++;
+			gameState.players[playerID] = new Player(playerID, players[playerID].name);
+			gameState.players[playerID].colorIndex = currentIndex;
+			this.#playerService.createPlayerModel(players[playerID], playerID);
+
+			// todo I don't think it's necessary to create the playerInterface object and save it for each player
+			//gameState.interfaces[playerID] = new PlayerInterface(playerID);
+
+			//}
+
+			if (playerID === myPlayer.id) {
+				this.#playerInterfaceService.createPlayerUI();
+			}
+		}
+	}
+
+	initializeGameField(mapType, gameId, myId) {
+		const gameField = this.#gameFieldService
+		gameField.createElement(mapType);
+
+		const game = this.#clientStore.games.get(gameId);
+
+		if (gameId === myId) {
+			// create and append start game button
+			gameField.createStartButton(game);
+		} else {
+			// create and append a message that says, "Waiting for the host to start the game..."
+		}
+	}
+
+	createGame(gameId, myId) {
+		const game  = this.#clientStore.games.get(gameId);
+
+		const players = game.state.players;
+		const settings = game.settings;
+
+		this.initializeGameField(settings.mapType, gameId, myId);
+		this.initializePlayers(gameId, players, players[myId]);
+		this.#gameInterfaceService.createGameUI(gameId, settings, players);
+		this.#clientManager.startRenderLoop();
+	}
+
+	startGame() {
+		this.#gameFieldService.hideLobby();
+	}
+
+	pauseGame() {
+
+	}
+
+	endGame(gameId) {
+		this.#gameFieldService.showScoreboard();
+		setTimeout(() => {
+			this.#clientManager.gameCleanup(gameId);
+		}, 1000)
+	}
+
+	leaveGame() {
+
 	}
 
 	updateGameState() {
