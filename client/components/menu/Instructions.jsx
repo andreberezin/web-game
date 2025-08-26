94863c61710c0cb083689d6499767ddf059fe75e
Andre Berezin <andreberezin@outlook.com>
2025-08-26 10:57:00 +0300

    WG-41 | edited instructions

--- client/components/menu/Instructions.jsx
+++ client/components/menu/Instructions.jsx
@@ -11,7 +11,7 @@ export function Instructions({setView}) {
 			<BackButton setView={setView} lastView={'main'}/>
 			<div id={'instructions-text'}>
 				<p>The object of the game is to kill your enemies</p>
-				<p>WASD keys to move</p>
+				<p>WASD or arrow keys to move</p>
 				<p>Mouse to aim</p>
 				<p>Left click to shoot</p>
 				<p>The game lasts until the game timer finishes or 1 player is left</p>
