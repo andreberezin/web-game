c534f566f827075729ad48a43aa99642afea3426
Andre Berezin <andreberezin@outlook.com>
2025-08-27 13:27:44 +0300

    WG-60 | refactored to default imports

--- server/di/container.js
+++ server/di/container.js
@@ -1,17 +1,19 @@
 import {asClass, createContainer} from 'awilix';
 import PlayerInputService from '../services/PlayerInputService.js';
 import GameService from '../services/GameService.js';
+import PlayerService from '../services/PlayerService.js';
 import SocketHandler from '../sockets/SocketHandler.js';
 import GamesManager from '../core/GamesManager.js';
 import serverStore from '../stores/serverStore.js';
-import {BulletService} from '../services/BulletService.js';
-import {PowerupService} from '../services/PowerupService.js';
+import BulletService from '../services/BulletService.js';
+import PowerupService from '../services/PowerupService.js';
 
 const container = createContainer()
 
 container.register({
 	playerInputService: asClass(PlayerInputService).singleton(),
 	gameService: asClass(GameService).singleton(),
+	playerService: asClass(PlayerService).singleton(),
 	bulletService: asClass(BulletService).singleton(),
 	powerupService: asClass(PowerupService).singleton(),
 	socketHandler: asClass(SocketHandler).singleton(),
