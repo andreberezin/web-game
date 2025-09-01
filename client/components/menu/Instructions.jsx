092f010e899683215c90f67fdb5c032cace0674f
Andre Berezin <andreberezin@outlook.com>
2025-09-01 16:37:24 +0300

    WG-84 | new instructions for scoring

--- client/components/menu/Instructions.jsx
+++ client/components/menu/Instructions.jsx
@@ -12,7 +12,7 @@ export function Instructions({setView}) {
 			<div id={'instructions-text'}>
 				<p>The object of the game is to kill your enemies.</p>
 				<p>The game lasts until the game timer finishes or 1 player is left.</p>
-				<p>Each kill gives you 1 point.</p>
+				<p>Each kill and each life left gives you 1 point.</p>
 				<p>The player with the most points or the last one alive wins the game.</p>
 				<p>Various power-ups spawn during the game which can be picked up.</p>
 				<p>Controls:
